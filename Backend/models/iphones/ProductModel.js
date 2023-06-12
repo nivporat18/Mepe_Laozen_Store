f77ec079e576c5d3919544c8b1b94e687f9598f7
Niv Porat <nivporat18@icloud.com>
2023-06-12 15:11:13 +0300

    update iphone 11 model

--- Backend/models/iphones/ProductModel.js
+++ Backend/models/iphones/ProductModel.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({
     name:{type:String,required:true},
-    brand:{type:String,required:true},
     desc:{type:String,required:true},
     price:{type:Number,required:true},
     image:{type:Object,required:true},
